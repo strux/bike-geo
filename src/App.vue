aa357319979bc7092f35a9e1d26b6c9983965626
strux <ben@carnivorous.org>
2018-10-31 09:51:04 -0700

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <v-app dark>
+        <v-navigation-drawer
+            v-model="drawer"
+            fixed
+            right
+            permanent
+            app
+            >
+            <v-list>
+                <v-list-tile
+                    v-for="bike in bikes"
+                    :key="bike.name"
+                    >
+                    <v-list-tile-content>
+                        <v-list-tile-title><span style="caption">{{bike.make}}</span> {{bike.model}}</v-list-tile-title>
+                    </v-list-tile-content>
+                    <v-list-tile-action>
+                        <v-switch color="red" v-model="bike.enabled"></v-switch>
+                    </v-list-tile-action>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
+        <v-content>
+            <geo-vizualizer></geo-vizualizer>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GeoVizualizer from './components/GeoVizualizer'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        GeoVizualizer
+    },
+    data () {
+        return {
+            bikes: [
+                { make: 'Evil', model: 'The Following', enabled: true },
+                { make: 'Evil', model: 'The Offering', enabled: false },
+            ]
+        }
+    }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/GeoVizualizer.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-container v-bind:style="{ height: containerHeight }">
+        <svg height="100%" width="100%" viewBox="0 0 50 50">
+            <circle r="25" cx="25" cy="25" fill="#f00" />
+        </svg>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+        containerHeight: '200px',
+    }),
+    beforeMount: function() {
+        this.containerHeight = window.innerHeight + 'px';
+    }
+  }
+</script>
+
+<style>
+
+</style>
